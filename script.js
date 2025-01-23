5315048413afebca24d562f1d391f5bf75176664
habitablebrandy9 <kknayak049@gmail.com>
2025-01-23 12:37:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ JavaScript to change text when button is clicked
+document.getElementById(changeTextBtn).addEventListener(click, function() {
+    document.querySelector(#content h2).innerText = You Clicked the Button!;
+});
